6ff971e09a19d826c854c87635673526ea8e0642
josenberg <mbx.philipe@gmail.com>
2016-12-01 16:08:01 -0200

    implementing data in the review

--- app.js
+++ app.js
@@ -86,6 +86,7 @@
     this.review = {};
 
     this.addReview = function(product) {
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
