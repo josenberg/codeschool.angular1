663bf92a5a1f567a616b71e395895c827dc47b23
josenberg <mbx.philipe@gmail.com>
2016-12-01 13:55:27 -0200

    Adding imagens and thumbnails

--- app.js
+++ app.js
@@ -6,13 +6,27 @@
       price: 2.95,
       description: '. . .',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [{
+        full: "images/dodecahedron-01-full.jpg",
+        thumb: "images/dodecahedron-01-thumb.jpg"
+      }, {
+        full: "images/dodecahedron-02-full.jpg",
+        thumb: "images/dodecahedron-02-thumb.jpg"
+      }]
     }, {
-        name: 'Pentagonal   Gem',
+        name: 'Pentagonal Gem',
         price: 5.95,
         description: '. . .',
         canPurchase: true,
-        soldOut: false
+        soldOut: false,
+        images: [{
+          full: "images/pentagonal-01-full.jpg",
+          thumb: "images/pentagonal-01-thumb.jpg"
+        }, {
+          full: "images/pentagonal-02-full.jpg",
+          thumb: "images/pentagonal-02-thumb.jpg"
+        }]
       }]
   // Note that the controller is inside the app module
   app.controller('StoreController', function() {
