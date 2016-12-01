cb59da9271a445a6f961fb464a368956f0a1842c
Jose Philipe Mendes de Godoy <vntjomg@suportes-imac.venturus.local>
2016-12-01 09:23:50 -0200

    Creating first controller and binding a scope of it in the html

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module("store", []);
+(function() {
+  var app = angular.module('store', []);
+
+  // Note that the controller is inside the app module
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: '. . .'
+  }
+})();
