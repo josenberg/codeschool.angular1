bfd818b1b0868635d619da5f3c2e4b5694055ca6
Jose Philipe Mendes de Godoy <vntjomg@suportes-imac.venturus.local>
2016-12-01 11:17:24 -0200

    using directives to handle state of the elements basead on its attributes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 (function() {
   var app = angular.module('store', []);
 
+  var gems = [{
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: '. . .',
+      canPurchase: true,
+      soldOut: false
+    }, {
+        name: 'Pentagonal   Gem',
+        price: 5.95,
+        description: '. . .',
+        canPurchase: true,
+        soldOut: false
+      }]
   // Note that the controller is inside the app module
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: '. . .'
-  }
+
 })();
